b0ac22825371368a664fa0dbd9df123222229e80
hai102cm <ngnghai1998@gmail.com>
2021-07-27 20:51:53 +0700

    edit name 2

--- config/env_setup.py
+++ config/env_setup.py
@@ -3,5 +3,5 @@ import os
 
 class EnvSetup:
     API = os.getenv('API', 'https://api.unsplash.com')
-    TOKEN = os.getenv('TOKEN', '9Fr3N7X0ngNl5xp2-t6x7ZNrEb1-UFq2At6cR4tSAmQ')
+    TOKEN = os.getenv('TOKEN', 'XVu_3P_7WQ_zEEH7v_66TWW8eN7YVhLTdcikzGRCIso')
 
